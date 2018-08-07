69ecd02563f8eb9f3471acd7b39f95ebc5a7cbdb
Paul Elion <paul.j.elion@gmail.com>
2018-08-07 20:46:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,8 @@ function removeElementFromBeginningOfArray(array){
   array.slice(1)
   return array
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
