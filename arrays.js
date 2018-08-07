4671a9916e7f3f42eb4d2507839499347620d635
Paul Elion <paul.j.elion@gmail.com>
2018-08-07 19:58:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element) {
-  array.push(element) = ;
-  return array
+function addElementToBeginningOfArray("array", "element") {
+  "array" = "array".push("element");
+  return "array"
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
