6929aaffd2027e4a2ca2c44d439760441d34a62e
Paul Elion <paul.j.elion@gmail.com>
2018-08-07 20:53:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice(array.length - 1) 
+  return array.slice(array.length - 1)
 }
